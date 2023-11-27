7b2a2a7c8f22ea6fffd02833d1ae1124da13018c
The Loom <loom@heytextile.com>
2023-11-26 22:18:17 -0600

    post test commit

---
+++ src/features/featureTest.js
@@ -0,0 +1,5 @@
+function feature() {
+  console.log('This is my feature test')
+}
+
+export default feature
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
+// import animateTitle from './features/animateTitle'
+// import createBadge from './features/createBasge'
 import './styles/style.css'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+// import feature from './features/featureTest'
 
-console.log('Welcome to Vite + JS + Webflow!')
+console.log('Welcome to the new Webflow workflow')
 
-createBadge()
-animateTitle()
+// createBadge()
+// animateTitle()
+
+// feature()
+
+const animation = gsap.to('.ball', {
+  x: '50vw',
+  borderRadius: '0%',
+  marginBottom: '2rem',
+  stagger: 0.5,
+  ease: 'ease',
+  yoyo: true,
+  repeat: -1,
+  paused: false,
+  // scrollTrigger: {
+  //   markers: true,
+  //   trigger: '.stream-wrapper',
+  // },
+  onComplete: () => {
+    console.log('completed')
+  },
+})
+
+const playButton = document.querySelector('[fs-element="play"]')
+
+const pauseButton = document.querySelector('[fs-element="pause"]')
+
+playButton.addEventListener('click', () => {
+  animation.play()
+})
+pauseButton.addEventListener('click', () => {
+  animation.pause()
+})
